c422c22c497d0c842405fe86feafa7ecb572ccd2
Aix <hl18770985367@163.com>
2025-09-29 17:40:36 +0800

    feat : /uploads 指向 http://localhost:3000

--- vite.config.ts
+++ vite.config.ts
@@ -29,6 +29,11 @@ export default defineConfig({
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path,
+      },
+      '/uploads':{
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        rewrite: (path) => path,
       }
     }
   }
